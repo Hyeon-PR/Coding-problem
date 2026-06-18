1411c979cd2e329c808654d631c244f109ceb37e
hyelee <lhbj1115@gmail.com>
2026-06-18 10:00:02 +0900

    Math - calculate angle between the hour and the minute hand

---
+++ Algorithm/math/Angle Between Hands of a Clock.py
@@ -0,0 +1,8 @@
+class Solution:
+    def angleClock(self, hour: int, minutes: int) -> float:
+
+        hour_angle = (hour * 30.0) + (minutes * 0.5)
+        minute_angle = minutes * 6.0
+
+        diff = abs(hour_angle - minute_angle)
+        return min(diff, 360.0 - diff)
